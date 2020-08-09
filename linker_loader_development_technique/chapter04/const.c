1ffc7362f4db40e7edf3320fad5425b9b03e1377
ashymonk <46420556+ashymonk@users.noreply.github.com>
2020-08-09 17:30:27 +0900

    linker_loader_development_technique: chapter04
    add const test.

---
+++ linker_loader_development_technique/chapter04/const.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int a;
+
+int main()
+{
+        a = 2;
+        printf("%d\n", a);
+        exit(0);
+}
---
+++ linker_loader_development_technique/chapter04/const_b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int b;
+
+int main()
+{
+        printf("%d\n", b);
+        exit(0);
+}
